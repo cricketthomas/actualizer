4d4d56bd47069c3561471685beab954f2b3e9735
Arthur Thomas <acthomasiv@gmail.com>
2020-01-26 19:59:25 -0500

    wip on adding paginated table

--- actualizer.ui/src/api/axios.js
+++ actualizer.ui/src/api/axios.js
@@ -1,5 +1,6 @@
 import axios from 'axios';
 import Vue from 'vue';
+import store from '../store/index.js';
 
 const axiosInstance = axios.create({
     baseURL: "https://localhost:5001/api/",
@@ -9,6 +10,7 @@ const axiosInstance = axios.create({
 axiosInstance.interceptors.request.use(async config => {
         config.headers.Authorization = `Bearer ${await Vue.prototype.$auth.getAccessToken() }`;
         return config;
+
     },
     error => Promise.reject(error));
 
@@ -19,23 +21,34 @@ axiosInstance.interceptors.response.use(async config => {
             return config;
         },
         error => {
-            Vue.prototype.$buefy.snackbar.open({
-                duration: 5000,
-                message: `Somethings up..${error}`,
-                type: 'is-danger',
+            // stop the loading screen bro..
+            store.state.comments.isLoading = false
+
+            let errorCode = parseInt(error.toString().match(/(\d+)/)[0])
+            let specialMessage = '';
+            switch (errorCode) {
+                case 400:
+                    specialMessage = 'is that the right video id..(error YG 400)'
+                    break;
+                case 500:
+                    specialMessage = 'somethings going down with my code on the backend..(error 500)'
+                    break;
+                case 401:
+                    specialMessage = 'you need to login..(error 401)'
+                    break;
+                case 403:
+                    specialMessage = "you don't have permissions to do that..(error 403)"
+                    break;
+                default:
+                    specialMessage = `${error}`
+                    break;
+            }
+            Vue.prototype.$buefy.toast.open({
+                duration: 4000,
+                message: `${specialMessage}`,
                 position: 'is-bottom-left',
-                actionText: 'Login',
-                queue: false,
-                onAction: () => {
-                    Vue.prototype.$buefy.toast.open({
-                        message: 'Action pressed',
-                        queue: false
-                    })
-                }
+                type: 'is-danger'
             })
-
-
-
         }
 
 )
--- actualizer.ui/src/components/explorer.vue
+++ actualizer.ui/src/components/explorer.vue
@@ -1,10 +1,13 @@
 <template>
     <div>
-        <input type="text" name="searchbox" id="searchbox" v-model="searchbox" />
-        <div v-for="(comment, key) in filteredComments" v-bind:key="key">
+        <!-- <input type="text" name="searchbox" id="searchbox" v-model="searchbox" /> -->
+        <!-- <div v-for="(comment, key) in filteredComments" v-bind:key="key">
             <p>{{ comment }}</p>
             <hr />
-        </div>
+
+        </div> -->
+    <b-table :data="results.documents" :columns="columns"> </b-table>
+
     </div>
 </template>
 
@@ -16,22 +19,27 @@ export default {
     props: {}, //make this take props
     data() {
         return {
-            searchbox: ''
+            searchbox: '',
+            columns: [
+                { field: 'id', label: 'id', width: '20', numeric: true },
+                { field: 'publishedAt', label: 'date', searchable: false },
+                { field: 'text', label: 'comment', searchable: true }
+            ]
         };
     },
     computed: {
-        filteredComments() {
-            if (this.results) {
-                let search = (this.searchbox || '').toLowerCase().trim();
-                let filtered = this.results.documents.filter(values => {
-                    let textsearch =  (values.text || "" ).toLowerCase();
-                    let IdSearch = values.id
-                    return  textsearch.indexOf(search) > -1 || IdSearch == parseInt(search);
-                });
-                return filtered;
-            }
-            return null;
-        },
+        // filteredComments() {
+        //     if (this.results) {
+        //         let search = (this.searchbox || '').toLowerCase().trim();
+        //         let filtered = this.results.documents.filter(values => {
+        //             let textsearch = (values.text || '').toLowerCase();
+        //             let IdSearch = values.id;
+        //             return textsearch.indexOf(search) > -1 || IdSearch == parseInt(search);
+        //         });
+        //         return filtered;
+        //     }
+        //     return null;
+        // },
         //return commentsArray.filter(stringVal => stringVal.indexOf(search) > -1 )
 
         ...mapGetters({
